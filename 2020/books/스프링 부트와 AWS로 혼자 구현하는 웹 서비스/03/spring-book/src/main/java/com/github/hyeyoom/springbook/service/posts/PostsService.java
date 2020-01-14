68dbbd31d4f83e081d29c799c133f1de7cec080d
hyeyoom <neoul_chw@icloud.com>
2020-01-15 01:08:08 +0900

    spring boot book

--- 2020/books/스프링 부트와 AWS로 혼자 구현하는 웹 서비스/03/spring-book/src/main/java/com/github/hyeyoom/springbook/service/posts/PostsService.java
+++ 2020/books/스프링 부트와 AWS로 혼자 구현하는 웹 서비스/03/spring-book/src/main/java/com/github/hyeyoom/springbook/service/posts/PostsService.java
@@ -3,11 +3,18 @@ package com.github.hyeyoom.springbook.service.posts;
 import com.github.hyeyoom.springbook.domain.posts.Posts;
 import com.github.hyeyoom.springbook.domain.posts.PostsRepository;
 import com.github.hyeyoom.springbook.web.dto.PostResponseDto;
+import com.github.hyeyoom.springbook.web.dto.PostsListResponseDto;
 import com.github.hyeyoom.springbook.web.dto.PostsSaveRequestsDto;
 import com.github.hyeyoom.springbook.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class PostsService {
@@ -18,14 +25,29 @@ public class PostsService {
         return postsRepository.save(requestsDto.toEntity()).getId();
     }
 
+    @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
         Posts post = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("No such posts: " + id));
         post.update(requestDto.getTitle(), requestDto.getContent());
         return post.getId();
     }
 
+    @Transactional
+    public void delete(Long id) {
+        Posts post = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("No such posts: " + id));
+        postsRepository.delete(post);
+    }
+
     public PostResponseDto findById(Long id) {
         Posts post = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("No such posts: " + id));
         return new PostResponseDto(post);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
 }
