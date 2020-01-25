c641fbe6ccf2861cd6182c445d78daa483dd8255
hyeyoom <neoul_chw@icloud.com>
2020-01-25 23:36:43 +0900

    unity doge

---
+++ 2020/books/2. unity/Dodge/Assets/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRigidBody;
+    public float speed = 8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+
+        float xSpeed = xInput * speed;
+        float ySpeed = yInput * speed;
+
+        Vector3 updatedV = new Vector3(xSpeed, 0f, ySpeed);
+        playerRigidBody.velocity = updatedV;
+    }
+
+    public void Die()
+    {
+        gameObject.SetActive(false);
+    }
+}
