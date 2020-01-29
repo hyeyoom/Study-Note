a623955fb283ace70cd4388c4ca31852846a6931
hyeyoom <neoul_chw@icloud.com>
2020-01-30 01:25:12 +0900

    unity

--- 2020/books/2. unity/Zombie/Scripts/PlayerMovement.cs
+++ 2020/books/2. unity/Zombie/Scripts/PlayerMovement.cs
@@ -1,4 +1,8 @@
-﻿using UnityEngine;
+﻿using System.Numerics;
+using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 // 플레이어 캐릭터를 사용자 입력에 따라 움직이는 스크립트
 public class PlayerMovement : MonoBehaviour
@@ -6,10 +10,11 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f; // 앞뒤 움직임의 속도
     public float rotateSpeed = 180f; // 좌우 회전 속도
 
-
     private PlayerInput playerInput; // 플레이어 입력을 알려주는 컴포넌트
     private Rigidbody playerRigidbody; // 플레이어 캐릭터의 리지드바디
     private Animator playerAnimator; // 플레이어 캐릭터의 애니메이터
+    private Camera cam;
+
     private static readonly int Move1 = Animator.StringToHash("Move");
 
     private void Start()
@@ -18,6 +23,7 @@ public class PlayerMovement : MonoBehaviour
         playerInput = GetComponent<PlayerInput>();
         playerRigidbody = GetComponent<Rigidbody>();
         playerAnimator = GetComponent<Animator>();
+        cam = Camera.main;
     }
 
     // FixedUpdate는 물리 갱신 주기에 맞춰 실행됨
@@ -40,7 +46,15 @@ public class PlayerMovement : MonoBehaviour
     // 입력값에 따라 캐릭터를 좌우로 회전
     private void Rotate()
     {
-        float turn = playerInput.rotate * rotateSpeed * Time.deltaTime;
-        playerRigidbody.rotation *= Quaternion.Euler(0, turn, 0f);
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Vector3 target = hit.point;
+            target.y = 0;
+            // target.y = transform.localScale.y / 2f;
+            playerRigidbody.transform.LookAt(target);
+        }
     }
 }
