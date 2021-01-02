eceff3a465494c9eb822e850ad5e3585cda2799c
hyeyoom <neoul_chw@icloud.com>
2021-01-02 17:16:21 +0900

    환경 구축

---
+++ 2021/lectures/data-jpa/src/main/java/com/github/hyeyoom/study/datajpa/DataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.github.hyeyoom.study.datajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataJpaApplication.class, args);
+    }
+
+}
