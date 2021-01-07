0c32fef4adb39aa6f76e8c8c020211d77877968c
hyeyoom <neoul_chw@icloud.com>
2021-01-07 22:01:31 +0900

    querydsl JPA 넘모 쉽당 ._.

---
+++ 2021/lectures/querydsl/src/test/java/com/github/hyeyoom/study/querydsl/entity/MemberTest.java
@@ -0,0 +1,50 @@
+package com.github.hyeyoom.study.querydsl.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@SpringBootTest
+@Transactional
+@Commit
+class MemberTest {
+
+    @Autowired
+    private EntityManager em;
+
+    @Test
+    void testEntity() throws Exception {
+        final Team teamA = new Team("teamA");
+        final Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        final Member m1 = new Member("m1", 10, teamA);
+        final Member m2 = new Member("m2", 20, teamA);
+
+        final Member m3 = new Member("m3", 30, teamB);
+        final Member m4 = new Member("m4", 40, teamB);
+        em.persist(m1);
+        em.persist(m2);
+        em.persist(m3);
+        em.persist(m4);
+
+        flushAndClear();
+
+        final List<Member> members = em.createQuery("SELECT m FROM Member m", Member.class).getResultList();
+        for (Member member : members) {
+            System.out.println("member = " + member);
+            System.out.println("member.team = " + member.getTeam());
+        }
+    }
+
+    private void flushAndClear() {
+        em.flush();
+        em.clear();
+    }
+}
